ed28d03db317bcecbef5f8a71fdd5ee2be234297
Julio Valls <julioevm@gmail.com>
2025-03-14 14:58:51 +0100

    chore: update music track and cover image in configuration

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -1,10 +1,10 @@
 import type { MusicData } from "~/types";
 
 const music: MusicData = {
-  title: "Sunflower",
-  artist: "Post Malone / Swae Lee",
-  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  title: "リサフランク420  現代のコンピュー ",
+  artist: "MACINTOSH PLUS",
+  cover: "img/ui/cover.jpeg",
+  audio: "music/macintosh.mp3"
 };
 
 export default music;
