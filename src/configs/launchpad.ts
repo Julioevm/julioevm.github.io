1ab14d4dfc8bc2a477b6809352b28a634354407e
Julio <38758281+Julioevm@users.noreply.github.com>
2025-06-10 08:49:52 +0000

    chore: Update projects and links.

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,22 +2,16 @@ import type { LaunchpadData } from "~/types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
+    id: "weeklio",
+    title: "Weeklio",
     img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
+    link: "https://getweeklio.com"
   },
   {
-    id: "oh-my-cv",
-    title: "Oh, My CV!",
+    id: "resume-flows",
+    title: "Resume Flows",
     img: "https://ohmycv.app/pwa-512x512.png",
-    link: "https://ohmycv.app"
-  },
-  {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/oh-vue-icons.png",
-    link: "https://oh-vue-icons.js.org"
+    link: "https://www.resumeflows.com"
   },
   {
     id: "gungnir",
@@ -25,41 +19,23 @@ const launchpadApps: LaunchpadData[] = [
     img: "img/icons/launchpad/gungnir.png",
     link: "https://vuepress-theme-gungnir.vercel.app/"
   },
-  // {
-  //   id: "metallic",
-  //   title: "Metallic",
-  //   img: "img/icons/launchpad/meta.png",
-  //   link: "https://github.com/Renovamen/metallic"
-  // },
   {
-    id: "what-if",
-    title: "My Notebook",
+    id: "scrum-poker",
+    title: "Scrum Poker",
     img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
-  },
-  {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
+    link: "http://scrum-poker-nine.vercel.app/"
   },
   {
     id: "resume",
     title: "Résumé",
     img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
+    link: "https://juliovalls.netlify.app/"
   },
   {
     id: "zelda",
     title: "Webpage for Zelda",
     img: "img/icons/launchpad/zelda.png",
     link: "https://zelda.zxh.io/"
-  },
-  {
-    id: "fishmail",
-    title: "Fishmail",
-    img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
   }
 ];
 
